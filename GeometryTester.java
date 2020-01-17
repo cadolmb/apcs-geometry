7f69b8fe0447f0b28e0bdc0237503d977a648b4d
Matthew <link0833@gmail.com>
2020-01-17 08:02:55 -0700

    fixed square

--- GeometryTester.java
+++ GeometryTester.java
@@ -5,7 +5,8 @@ class GeometryTester {
         Point b = new Point(15, 26);
         Point c = new Point(31, 15);
         Triangle triangle = new Triangle(a, b, c);
-
+        Square square = new Square(a, 20, 30);
+        
         System.out.println(triangle.area());
         System.out.println(Point.distance(a, b));
     }
--- Square.java
+++ Square.java
@@ -1,42 +1,20 @@
 public class Square {
 
     private Point a;
-    private Point b;
-    private Point c;
-    private Poind d;
+    private double width;
+    private double height;
 
-    public Square(Point a, Point b, Point c, Point d) {
+    public Square(Point a, width, height) {
         this.a = a;
-        this.b = b;
-        this.c = c;
-        this.d = d;
+        this.width = width;
+        this.height = height;
     }
 
     public double area() {
-
-        // Find which line segments are base and height
-        double ab = a.distance(b);
-        double ac = a.distance(c);
-        double ad = a.distance(c);
-
-        double hypot = ab;
-        double base = ab;
-        double height = ab;
-
-        if (ac > hypot) {
-            hypot = ac;
-        }
-        else {
-            base = ac;
-        }
-        if (ad > hypot) {
-            hypot = ad;
-        }
-        else {
-            base = ad;
-        }
-
-        return base*height;
+        return base * height;
     }
 
+    public double perimiter() {
+        return 2 * base + 2 * height;
+    }
 }
