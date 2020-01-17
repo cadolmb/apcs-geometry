6d9174bb2b2e1ba32efdd345aa1466cd1579f7e2
Matthew Cadol <cadolmb@s.dcsdk12.org>
2020-01-17 10:35:25 -0700

    simplified square intersect

--- GeometryTester.java
+++ GeometryTester.java
@@ -1,13 +1,23 @@
 class GeometryTester {
 
     public static void main(String[] args) {
-        Point a = new Point(8, 11);
-        Point b = new Point(15, 26);
-        Point c = new Point(31, 15);
+
+        System.out.println("\nGeometry Tester!\n");
+
+        Point a = new Point(0, 0);
+        Point b = new Point(5, 5);
+        Point c = new Point(4, 6);
         Triangle triangle = new Triangle(a, b, c);
-        Square square = new Square(a, 20, 30);
-        
-        System.out.println(triangle.area());
-        System.out.println(Point.distance(a, b));
+
+        System.out.println("area of triangle: " + triangle.area());
+        System.out.println("distance between a and b: " + Point.distance(a, b));
+
+        Square s1 = new Square(a, 20, 30);
+        Square s2 = new Square(b, 10, 10);
+
+        System.out.println("\narea of s1: " + s1.area());
+        System.out.println("perimiter of s2: " + s2.perimeter());
+
+        System.out.println("\ndo s1 and s2 intersect? " + Square.squaresIntersect(s1, s2));
     }
 }
--- Square.java
+++ Square.java
@@ -1,62 +1,45 @@
 public class Square {
 
-    private Point[] points = new Point[4];
-
+    private Point p;
     private double width;
     private double height;
 
-    public Square(Point p, width, height) {
+    public Square(Point p, double width, double height) {
+        this.p = p;
         this.width = width;
         this.height = height;
-
-        point[0] = p;
-        points[1] = new Point(p1.x + width, p1.y);
-        points[2] = new Point(p1.x + width, p1.y + height);
-        points[3] = new Point(p1.x, p1.y + height);
-
     }
 
     public double area() {
-        return base * height;
+        return width * height;
     }
 
-    public double perimiter() {
-        return 2 * base + 2 * height;
-    }
-
-    public Point[] points() {
-        return points;
+    public double perimeter() {
+        return 2 * width + 2 * height;
     }
 
     public double xLeft() {
-        points[0].x;
+        return p.x;
     }
 
     public double xRight() {
-        points[0].x + width;
+        return p.x + width;
     }
 
     public double yBottom() {
-        points[0].y;
+        return p.y;
     }
 
     public double yTop() {
-        points[0].y + height;;
+        return p.y + height;
     }
 
     public static boolean squaresIntersect(Square s1, Square s2) {
-        for (Point p : s1.points()) {
-            if insideSquare(p, s2) {
-                return true;
-            }
-        }
-        return false;
+        if ( s1.xRight() < s2.xLeft() ) return false;
+        if ( s1.xLeft() > s2.xRight() ) return false;
+        if ( s1.yBottom() > s2.yTop() ) return false;
+        if ( s1.yTop() < s2.yBottom() ) return false;
+        return true;
     }
 
-    public static boolean insideSquare(Point p, Square s) {
-        if (p.x > s.xLeft() && p.x < s.xRight()) && (p.y > s.yBottom() && p.y < s.yTop) {
-            return true;
-        }
-        return false;
-    }
 }
