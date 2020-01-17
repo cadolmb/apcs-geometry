9b6bbd430370a94433f4e6faf6cb95306f17e337
Matthew <link0833@gmail.com>
2020-01-17 08:28:07 -0700

    square incomplete

--- Square.java
+++ Square.java
@@ -1,13 +1,19 @@
 public class Square {
 
-    private Point a;
+    private Point[] points = new Point[4];
+
     private double width;
     private double height;
 
-    public Square(Point a, width, height) {
-        this.a = a;
+    public Square(Point p, width, height) {
         this.width = width;
         this.height = height;
+
+        point[0] = p;
+        points[1] = new Point(p1.x + width, p1.y);
+        points[2] = new Point(p1.x + width, p1.y + height);
+        points[3] = new Point(p1.x, p1.y + height);
+
     }
 
     public double area() {
@@ -16,5 +22,41 @@ public class Square {
 
     public double perimiter() {
         return 2 * base + 2 * height;
+    }
+
+    public Point[] points() {
+        return points;
+    }
+
+    public double xLeft() {
+        points[0].x;
+    }
+
+    public double xRight() {
+        points[0].x + width;
+    }
+
+    public double yBottom() {
+        points[0].y;
+    }
+
+    public double yTop() {
+        points[0].y + height;;
+    }
+
+    public static boolean squaresIntersect(Square s1, Square s2) {
+        for (Point p : s1.points()) {
+            if insideSquare(p, s2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean insideSquare(Point p, Square s) {
+        if (p.x > s.xLeft() && p.x < s.xRight()) && (p.y > s.yBottom() && p.y < s.yTop) {
+            return true;
+        }
+        return false;
     }
 }
